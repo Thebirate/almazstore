543b50a4d3f3034f3c9f9d3deeea4e6dd5da6cb8
Thebirate <60749357+Thebirate@users.noreply.github.com>
2025-07-16 10:57:40 -0700

    Removed searchAction and checkoutAction imports

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ import {
   SingleProduct,
   UserProfile,
 } from "./pages";
-import { checkoutAction, searchAction } from "./actions/index";
+//import { checkoutAction, searchAction } from "./actions/index";
 import { shopCategoryLoader } from "./pages/Shop";
 import { loader as orderHistoryLoader } from "./pages/OrderHistory";
 import { loader as singleOrderLoader } from "./pages/SingleOrderHistory";
@@ -48,11 +48,11 @@ const router = createBrowserRouter([
       {
         path: "checkout",
         element: <Checkout />,
-        action: checkoutAction,
+       // action: checkoutAction,
       },
       {
         path: "search",
-        action: searchAction,
+        //action: searchAction,
         element: <Search />,
       },
       {
